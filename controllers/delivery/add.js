a80c8b32eaf085d70caa60b7f6676f5a57be5328
Memory <nguyenlamspk@gmail.com>
2017-10-12 23:23:23 +0700

    delivery

---
+++ controllers/delivery/add.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var model = require('./../../models/delivery.model');
+var API = require('./../../APILib');
+var utils = require('./../../utils');
+
+module.exports = (req, res) => {
+  var data = req.body;
+  var authInfo = utils.getAuthInfo(req.headers.authorization);
+  data.createdBy = authInfo._id;
+  var objData = new model(data);
+
+  objData.save(function (err) {
+    var result = {
+      "statusCode": 0,
+      "message": "Success"
+    }
+    if (err) {
+      result.statusCode = -1;
+      result.message = "Error";
+    }
+    res.json(result);
+  });
+
+};
