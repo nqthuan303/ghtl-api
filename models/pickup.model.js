c1226c4e7807799c141495c43e49f29dcc5e5baa
Thuan Nguyen <nqthuan303@gmail.com>
2018-02-05 20:57:32 +0700

    change constants file name

--- models/pickup.model.js
+++ models/pickup.model.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 var counter = require('./counter.model');
+const status = require('../constants/status');
 
 var objSchema = new Schema({
     id: String,
@@ -10,7 +11,12 @@ var objSchema = new Schema({
         id: {type: ObjectId, ref: 'client', required: true},
         orders: [{type: ObjectId, ref: 'order', required: true}]
     }],
-    status: '',
+    status: {
+        type: String, 
+        required: true, 
+        enum: [status.pickup.PENDING, status.pickup.DOING, status.pickup.DONE],
+        default: status.pickup.PENDING
+    },
     createdBy: {type: ObjectId, ref: 'user', required: true },
     updatedBy: [{type: ObjectId, ref: 'user'}]
 }, { timestamps: true });
