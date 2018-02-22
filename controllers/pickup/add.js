f04f532779d013905d7be9e6e7e3b72c2d09539d
Thuan Nguyen <nqthuan303@gmail.com>
2018-02-22 21:34:03 +0700

    fix add pickup

--- controllers/pickup/add.js
+++ controllers/pickup/add.js
@@ -1,34 +1,43 @@
 'use strict';
 
-const model = require('./../../models/pickup.model');
+const PickupModel = require('./../../models/pickup.model');
 const orderModel = require('./../../models/order.model');
 const orderStatusModel = require('./../../models/orderStatus.model');
 const API = require('./../../APILib');
-const utils = require('./../../utils');
 const status = require('../../constants/status');
 
 module.exports = async (req, res) => {
   const data = req.body;
-  const authInfo = utils.getAuthInfo(req.headers.authorization);
-  data.createdBy = authInfo._id;
-  const objData = new model(data);
 
   try {
-    const saveOrder = await objData.save();
-    const pickup = await orderStatusModel.findOne({value: status.order.PREPAREDELIVERY});
-    const pickupId = pickup._id;
+    //tìm chuyến đi có trạng thái là pending và thuộc về shiper cần tạo
+    const pickupTrip = await PickupModel.findOne({status: status.pickup.PENDING, user: data.user});
 
-    const updateOrder = await orderModel.update(
-      { _id : { $in : data.orders }}, 
-      { orderstatus: pickup},
-      {"multi": true}
-    )
+    if(pickupTrip){
+      //Nếu đã tồn tại thì cập nhật lại chuyến đi đó
+      const {data: pickupData} = pickupTrip;
+      const updateData = JSON.parse(JSON.stringify(pickupTrip.data));
+      updateData.push({
+        clientId: data.client,
+        orders: data.orders
+      });
+      const updateResult = await PickupModel.findByIdAndUpdate(pickupTrip._id, {data: updateData});
+    }else{
+      //Nếu chưa thì tạo mới chuyến đi
+      const addData = {
+        user: data.user,
+        data: [{
+          clientId: data.client,
+          orders: data.orders
+        }]
+      };
+      const objAdd = new PickupModel(addData);
+      const addResult = await objAdd.save(req); 
+    }
+    API.success(res, {});
 
-    API.success(res, {
-        message: 'Success!'
-    });
   } catch (err) {
-    return API.fail(res, err.message);
+    API.fail(res, err.message);
   }
 
 };
