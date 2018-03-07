fc8ef0cf1ab1a5543f651a6ab254cc6bfe1481b4
Thuan Nguyen <nqthuan303@gmail.com>
2018-03-07 21:14:56 +0700

    UPDATE ORDERSTATUS

--- constants/status.js
+++ constants/status.js
@@ -1,24 +1,71 @@
 
+module.exports 
 const status = {
     order: {
-      TEMP: 'tạm', // trạng thái khi khách hàng tạo đơn hàng nhưng chưa xác nhận
-      PENDING: 'pending', // đơn hàng sau khi khách hàng xác nhận
-      PREPAREPICKUP: 'preparePickup', // Chuẩn bị lấy hàng
-      PICKUP: 'pickup', // Lấy hàng
-      CANCEL: 'cancel', // Chưa lấy hàng về kho và khách hàng yêu cầu hủy
-      STORAGE: 'storage', // Lưu kho
-      PREPARE_DELIVERY: 'prepareDelivery', // Chuẩn bị giao hàng
-      DELIVERY: 'delivery', // Giao hàng
-      REFUND_STORAGE: 'refundStorage', // Hàng bị trả đang lưu kho sẽ gửi lại cho khách (có tính phí)
-      PREPARE_REFUND_RETURNING: 'prepareRefundReturn', // Hàng bị trả đang được chuẩn bị để trả cho khách
-      REFUND_RETURNING: 'refundReturning', // Hàng bị trả đang được gửi lại cho khách
-      REFUND_NOSTORAGE: 'refundNoStorage', // Hàng bị trả đã được gửi lại cho khách
-      CANCEL_STORAGE: 'cancelStorage', // Hàng bị hủy đang được lưu kho
-      CANCEL_RETURNING: 'cancelReturning', // Hàng bị hủy đang được gửi trả lại cho khách
-      PREPARE_CANCEL_RETURNING: 'prepareCancelReturn', // Hàng bị hủy đang được chuẩn bị để trả cho khách
-      CANCEL_NOSTORAGE: 'cancelNoStorage', // Hàng bị hủy đã được gửi lại cho khách
-      DONE: 'done',
-      PAID: 'paid',
+      TEMP: { // trạng thái khi khách hàng tạo đơn hàng nhưng chưa xác nhận
+        name: 'Tạm',
+        value: 'temp'
+      },
+      PENDING: { // đơn hàng đang đợi xử lý
+        name: 'Chờ lấy hàng',
+        value: 'pending'
+      },
+      CANCEL: { // Chưa lấy hàng về kho và khách hàng yêu cầu hủy
+        name: 'Hủy',
+        value: 'cancel'
+      },
+      PICKUP: {
+        name: 'Đang lấy hàng',
+        value: 'pickup'
+      },
+      STORAGE: {
+        name: 'Lưu kho',
+        value: 'storage'
+      },
+      DELIVERYPREPARE: {
+        name: 'Chuẩn bị giao hàng',
+        value: 'deliveryPrepare'
+      },
+      DELIVERY: {
+        name: 'Đang giao hàng',
+        value: 'delivery'
+      },
+      DELIVERED: {
+        name: 'Đã giao',
+        value: 'delivered'
+      },
+      RETURNFEESTORAGE: { //Hàng trả có phí đang được lưu kho
+        name: 'Trả (phí) - lưu kho',
+        value: 'returnFeeStorage'
+      },
+      RETURNFEEPREPARE: { //Hàng trả có phí (chuẩn bị trả)
+        name: 'Trả (phí) - Chuẩn bị trả',
+        value: 'returnFeePrepare'
+      },
+      RETURNINGFEE: { //Hàng trả có phí (đang trả)
+        name: 'Trả (phí) - Đang trả',
+        value: 'returningFee'
+      },
+      RETURNEDFEE: { //Hàng trả có phí (đã trả)
+        name: 'Trả (phí) - Đã trả',
+        value: 'returnedFee'
+      },
+      RETURNSTORAGE: { //hàng trả không phí (lưu kho)
+        name: 'Trả - lưu kho',
+        value: 'returnStorage'
+      },
+      RETURNPREPARE: { //Hàng trả không phí (chuẩn bị trả)
+        name: 'Trả - Chuẩn bị trả',
+        value: 'returnPrepare'
+      },
+      RETURNING: { //Hàng trả không phí (đang trả)
+        name: 'Trả - Đang trả',
+        value: 'returning'
+      },
+      RETURNED: { //Hàng trả không phí (đã trả)
+        name: 'Trả - Đã trả',
+        value: 'returned'
+      }
     },
     pickup: {
       PENDING: 'pending',
@@ -37,4 +84,4 @@ const status = {
     },
   };
   
-  module.exports = status;  
+  module.exports = status;
