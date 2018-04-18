cfaa5309b02ff1dbdfeb61757553d058f629f724
Thuan Nguyen <nqthuan303@gmail.com>
2018-04-18 23:08:24 +0700

    change populate pickup/list

--- controllers/pickup/list.js
+++ controllers/pickup/list.js
@@ -5,31 +5,44 @@ var API = require('./../../APILib');
 
 module.exports = async (req, res) => {
     var objQuery = req.query;
-
-    try {
-        const pickUps = await model.find()
-        .populate({
-            path: 'client',
-            select: 'name address phone district',
-            populate: { 
-                path: 'district',
-                select: 'name type',
-            }
-        })
-        .populate({
+    const populateOpt = [
+        {
+            path: 'clients',
+            select: 'id name address phone district ward orders',
+            populate: [
+                {
+                    path: 'district',
+                    select: 'type name'
+                },
+                {
+                    path: 'ward',
+                    select: 'type name'
+                },
+            ]
+        },
+        {
             path: 'data',
             populate: {
                 path: 'clientId'
             }
-        })
-        .populate('shipper', 'name phone id')
-        .populate('orders', 'id')
-        .sort({
-            'createdAt': -1,
-            'user': -1
-        });
-        API.success(res, pickUps);
+        },
+        {
+            path: 'shipper',
+            select: 'name phone id'
+        },
+        {
+            path: 'orders',
+            select: 'id'
+        }
+    ];
+    const sortOpt = {
+        'createdAt': -1,
+        'user': -1
+    };
 
+    try {
+        const pickUps = await model.find().populate(populateOpt).sort(sortOpt);
+        API.success(res, pickUps);
     } catch (error) {
         API.fail(res, error.message);
     }
